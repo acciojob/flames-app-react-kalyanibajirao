7a449c396e8d67224dddbe0be7b4cd80a8355121
kalyanibajirao <kalyanibajirao1999@gmail.com>
2024-09-03 20:34:09 +0530

    updated code

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,75 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { useState } from "react";
 
-class App extends Component {
-    render() {
+const App = () => {
+  const [name1, setName1] = useState("");
+  const [name2, setName2] = useState("");
+  const [result, setResult] = useState("");
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
+  const calculateRelationship = () => {
+    if (!name1 || !name2) {
+      setResult("Please Enter valid input");
+      return;
     }
-}
 
+    let name1Arr = name1.split("");
+    let name2Arr = name2.split("");
+
+    name1Arr.forEach((char) => {
+      const index = name2Arr.indexOf(char);
+      if (index !== -1) {
+        name2Arr.splice(index, 1);
+        name1Arr.splice(name1Arr.indexOf(char), 1);
+      }
+    });
+
+    const remainingLength = name1Arr.length + name2Arr.length;
+    const flames = [
+      "Siblings",
+      "Friends",
+      "Love",
+      "Affection",
+      "Marriage",
+      "Enemy",
+    ];
+    const relationship = flames[remainingLength % 6];
+
+    setResult(relationship);
+  };
+
+  const clearFields = () => {
+    setName1("");
+    setName2("");
+    setResult("");
+  };
+
+  return (
+    <div>
+      <input
+        data-testid="input1"
+        type="text"
+        value={name1}
+        onChange={(e) => setName1(e.target.value)}
+        placeholder="Enter first name"
+      />
+      <input
+        data-testid="input2"
+        type="text"
+        value={name2}
+        onChange={(e) => setName2(e.target.value)}
+        placeholder="Enter second name"
+      />
+      <button
+        data-testid="calculate_relationship"
+        onClick={calculateRelationship}
+      >
+        Calculate Relationship
+      </button>
+      <button data-testid="clear" onClick={clearFields}>
+        Clear
+      </button>
+      <h3 data-testid="answer">{result}</h3>
+    </div>
+  );
+};
 
 export default App;
